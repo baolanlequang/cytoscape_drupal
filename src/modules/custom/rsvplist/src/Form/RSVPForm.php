521c82b3ab3773af295b0db97f70f354efe1ad65
Bao Lan Le Quang <baolan2005@gmail.com>
2020-07-13 23:34:11 +0700

    add permission

--- src/modules/custom/rsvplist/src/Form/RSVPForm.php
+++ src/modules/custom/rsvplist/src/Form/RSVPForm.php
@@ -5,7 +5,7 @@
  */
 namespace Drupal\rsvplist\Form;
 
-use Drupal\Core\Database\Database;
+use Drupal\Core\Database\Connection;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 
@@ -24,9 +24,8 @@ class RSVPForm extends FormBase {
      * (@inheritdoc)
      */
     public function buildForm(array $form, FormStateInterface $form_state) {
-        // $node = \Drupal::routeMatch()->getParameter('node');
-        // $nid = $node->nid->value;
-        $nid = '';
+        $node = \Drupal::routeMatch()->getParameter('node');
+        $nid = $node->nid->value;
         $form['email'] = array(
             '#title' => t('Email Address'),
             '#type' => 'textfield',
@@ -48,8 +47,27 @@ class RSVPForm extends FormBase {
     /**
      * (@inheritdoc)
      */
+    public function validateForm(array &$form, FormStateInterface $form_state) {
+        $value = $form_state->getValue('email');
+        if ($value == !\Drupal::service('email.validator')->isValid($value)) {
+            $form_state->setErrorByName('email', t('The email address %mail is not valid.', array('%mail' => $value)));
+        }
+    }
+
+    /**
+     * (@inheritdoc)
+     */
     public function submitForm(array &$form, FormStateInterface $form_state) {
         $messenger_service = \Drupal::service('messenger');
-        $messenger_service->addMessage(t('The form is working.'));
+        
+        $user = \Drupal\user\Entity\User::load(\Drupal::currentUser()->id());
+        $connection = \Drupal::database();
+        $connection->insert('rsvplist')->fields(array(
+            'mail' => $form_state->getValue('email'),
+            'nid' => $form_state->getValue('nid'),
+            'uid' => $user->id(),
+            'created' => time(),
+        ))->execute();
+        $messenger_service->addMessage(t('Thank for your RSVP, you are on the list for the event.'));
     }
 }
