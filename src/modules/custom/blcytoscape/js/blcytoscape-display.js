a2b5cae56f922826c265a0a5b3a2c6b49e639b52
Bao Lan Le Quang <baolan2005@gmail.com>
2020-07-25 14:52:08 +0700

    update module

--- src/modules/custom/blcytoscape/js/blcytoscape-display.js
+++ src/modules/custom/blcytoscape/js/blcytoscape-display.js
@@ -3,53 +3,86 @@
  * Displays cytoscape
  */
 
- (function ($, Drupal) {
+ (function ($, Drupal, drupalSettings) {
      'use strict';
      Drupal.behaviors.csdisplay = {
         attach: function(context, settings) {
-            $('#cy', context).once('csdisplay').each(function() {
-                console.log('ahihi');
-                var cy = cytoscape({
-                    container: $('#cy'),
-                    elements: [ // list of graph elements to start with
-                        { // node a
-                          data: { id: 'a' }
-                        },
-                        { // node b
-                          data: { id: 'b' }
-                        },
-                        { // edge ab
-                          data: { id: 'ab', source: 'a', target: 'b' }
-                        }
-                      ],
-                    
-                      style: [ // the stylesheet for the graph
-                        {
-                          selector: 'node',
-                          style: {
-                            'background-color': '#666',
-                            'label': 'data(id)'
-                          }
-                        },
-                    
-                        {
-                          selector: 'edge',
-                          style: {
-                            'width': 3,
-                            'line-color': '#ccc',
-                            'target-arrow-color': '#ccc',
-                            'target-arrow-shape': 'triangle',
-                            'curve-style': 'bezier'
-                          }
-                        }
-                      ],
-                    
-                      layout: {
-                        name: 'grid',
-                        rows: 1
-                      } 
-                });
+            $('.cy', context).once('csdisplay').each(function() {
+              console.log(drupalSettings.cytoscape.elements)
+              var cy = cytoscape({
+                container: $('.cy'),
+                elements: drupalSettings.cytoscape.elements,
+                style: drupalSettings.cytoscape.style,
+                layout: drupalSettings.cytoscape.layout,
+                // initial viewport state:
+                zoom: 1,
+                pan: { x: 0, y: 0 },
+                // interaction options:
+                minZoom: 1e-50,
+                maxZoom: 1e50,
+                zoomingEnabled: true,
+                userZoomingEnabled: true,
+                panningEnabled: true,
+                userPanningEnabled: true,
+                boxSelectionEnabled: true,
+                selectionType: 'single',
+                touchTapThreshold: 8,
+                desktopTapThreshold: 4,
+                autolock: false,
+                autoungrabify: false,
+                autounselectify: false,
+
+                // rendering options:
+                headless: false,
+                styleEnabled: true,
+                hideEdgesOnViewport: false,
+                textureOnViewport: false,
+                motionBlur: false,
+                motionBlurOpacity: 0.2,
+                wheelSensitivity: 1,
+                pixelRatio: 'auto'
+              });
             });
+
+            //   var cy = cytoscape({
+            //     container: $('.cy'),
+            //     elements: [ // list of graph elements to start with
+            //       { // node a
+            //         data: { id: 'a' }
+            //       },
+            //       { // node b
+            //         data: { id: 'b' }
+            //       },
+            //       { // edge ab
+            //         data: { id: 'ab', source: 'a', target: 'b' }
+            //       }
+            //     ],
+            //     style: [ // the stylesheet for the graph
+            //       {
+            //         selector: 'node',
+            //         style: {
+            //           'background-color': '#666',
+            //           'label': 'data(id)'
+            //         }
+            //       },
+            //       {
+            //         selector: 'edge',
+            //         style: {
+            //           'width': 3,
+            //           'line-color': '#ccc',
+            //           'target-arrow-color': '#ccc',
+            //           'target-arrow-shape': 'triangle',
+            //           'curve-style': 'bezier'
+            //         }
+            //       }
+            //     ],
+            //     layout: {
+            //       name: 'grid',
+            //       rows: 1
+            //     } 
+            //   });
+
+            // });
         }
     };
- })(jQuery, Drupal);
+ })(jQuery, Drupal, drupalSettings);
--- src/modules/custom/blcytoscape/src/Controller/BLCSDisplayController.php
+++ src/modules/custom/blcytoscape/src/Controller/BLCSDisplayController.php
@@ -11,10 +11,38 @@ class BLCSDisplayController extends ControllerBase {
     public function content() {
         $build = array(
             '#type' => 'markup',
-            '#markup' => '<div id="cy">' . $this->t('This is my BLCSDisplayController') . '</div>',
+            '#markup' => '<div class="cy">' . $this->t('This is my BLCSDisplayController') . '</div>',
         );
         $build['#attached']['library'][] = 'blcytoscape/cytoscapelib';
         $build['#attached']['library'][] = 'blcytoscape/blcytoscape';
+        $build['#attached']['drupalSettings']['cytoscape']['elements'] = [
+            ['data'=> ['id'=>'a']],
+            ['data'=> ['id'=>'b']],
+            ['data'=> ['id'=>'ab', 'source'=>'a', 'target'=>'b']],
+        ];
+        $build['#attached']['drupalSettings']['cytoscape']['style'] = [
+            [
+                'selector'=>'node', 
+                'style'=>[
+                    'background-color'=>'#666',
+                    'label'=>'data(id)'
+                ],
+            ],
+            [
+                'selector'=>'edge', 
+                'style'=>[
+                    'width'=>3,
+                    'line-color'=>'#ccc',
+                    'target-arrow-color'=>'#ccc',
+                    'target-arrow-shape'=>'triangle',
+                    'curve-style'=>'bezier'
+                ],
+            ],
+        ];
+        $build['#attached']['drupalSettings']['cytoscape']['layout'] = [
+            'name'=>'grid',
+            'rows'=>1,
+        ];
         return $build;
     }
 }
