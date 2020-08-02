282834044a184c754eaa9bc01061a40455a50159
Bao Lan Le Quang <baolan2005@gmail.com>
2020-08-02 19:16:31 +0700

    update source code

--- src/modules/custom/blcytoscape/src/Form/BLCSSettingDisplayForm.php
+++ src/modules/custom/blcytoscape/src/Form/BLCSSettingDisplayForm.php
@@ -19,12 +19,19 @@ class BLCSSettingDisplayForm extends ConfigFormBase {
     }
 
     /**
+     * {@inheritdoc}
+     */
+    protected function getEditableConfigNames() {
+        return ['blcytoscape.settings'];
+    }
+
+    /**
      * 
      */
     protected function loadListGraphs() {
         $select = Database::getConnection()->select('blcytoscape', 'blcy');
         $select
-            ->fields('blcy', ['elements', 'nid']);
+            ->fields('blcy', ['nid']);
         $entries = $select->execute()->fetchAll(\PDO::FETCH_ASSOC);
         return $entries;
     }
@@ -47,11 +54,29 @@ class BLCSSettingDisplayForm extends ConfigFormBase {
         //     '#value' => TRUE
         // );
         $list_graphs = $this->loadListGraphs();
+        // var_dump(json_decode($list_graphs[0]['elements']));
         $form['blcytoscape_graph_list'] = array(
             '#title' => t('List of graphs'),
             '#type' => 'select',
             '#options' => $list_graphs,
         );
         return parent::buildForm($form, $form_state);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function submitForm(array &$form, FormStateInterface $form_state) {
+        $messenger_service = \Drupal::service('messenger');
+        
+        // $user = \Drupal\user\Entity\User::load(\Drupal::currentUser()->id());
+        // $connection = \Drupal::database();
+        // $connection->insert('rsvplist')->fields(array(
+        //     'mail' => $form_state->getValue('email'),
+        //     'nid' => $form_state->getValue('nid'),
+        //     'uid' => $user->id(),
+        //     'created' => time(),
+        // ))->execute();
+        $messenger_service->addMessage(t('Thank for your RSVP, you are on the list for the event.'));
     }
 }
